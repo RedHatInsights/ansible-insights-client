3e8f25aa01cf7253a7bfbf45bb2e7b0aa4ee9190
Kyle Lape <klape@redhat.com>
2017-03-17 17:04:16 -0500

    Adding code showing how to update insights module from action plugin
    The thinking here is that we replace the existing insights.py on the
    local disk with what we download from Red Hat.  The big disadvantage is
    that everything has to be in one big file.  This is a limitation of
    Ansible:
    
    > Modules must be self-contained in one file to be auto-transferred by ansible
    
    (http://docs.ansible.com/ansible/dev_guide/developing_modules_best_practices.html)
    
    Perhaps we can do a manual transfer, but I'm not sure of the
    compatibility implications, and it kind of reeks of a hack.

--- action_plugins/insights.py
+++ action_plugins/insights.py
@@ -1,17 +1,27 @@
-import requests
 from ansible.plugins.action import ActionBase
 from ansible.utils.vars import merge_hash
+
+
+def update_code(module_location):
+    """
+    Let's just see if we can execute a module "generated" on-the-fly by an
+    action plugin
+    """
+    with open("insights/insights.py") as fp:
+        content = fp.read()
+    content = content.replace("regoob", "Content updated!")
+    with open(module_location, "w") as fp:
+        fp.write(content)
 
 
 class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
-
         results = super(ActionModule, self).run(tmp, task_vars)
-        results = merge_hash(results, self._execute_module(tmp=tmp, task_vars=task_vars))
-        r = requests.get("https://google.com/")
+        module_location = self._shared_loader_obj.module_loader.find_plugin("insights")
+        update_code(module_location)
+        results = merge_hash(results, self._execute_module(module_name="insights", tmp=tmp, task_vars=task_vars))
         results.update({
-            "shi": "kaka",
-            "status_code": str(r.status_code)
+            "shi": "kaka"
         })
         return results
