aabb7f41712f9c005ebb32fdf52cf8bdd08d373b
Jesse Jaggars <jjaggars@redhat.com>
2017-04-20 17:41:14 -0500

    Can execute the module directly with python as well
    Removed the dynamic rewriting of the module
    factored out the data gathering into a function

--- insights/insights.py
+++ insights/insights.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
-from ansible.module_utils.basic import AnsibleModule
+
+
+def get_content():
+    return {"booger": "Content updated!"}
+
+
+def noansible():
+    import json
+    print json.dumps(get_content())
 
 
 def main():
+    from ansible.module_utils.basic import AnsibleModule
     module = AnsibleModule({})
-    module.exit_json(booger="regoob")
+    module.exit_json(**get_content())
 
 if __name__ == "__main__":
-    main()
+    noansible()
