1a56bd7e857bc8113ff639f7e6aff6960fd9d9c4
Jesse Jaggars <jjaggars@redhat.com>
2017-04-24 16:48:24 -0500

    push an egg and run it

--- action_plugins/insights.py
+++ action_plugins/insights.py
@@ -6,8 +6,12 @@ class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
         results = super(ActionModule, self).run(tmp, task_vars)
-        results = merge_hash(results, self._execute_module(module_name="insights", tmp=tmp, task_vars=task_vars))
-        results.update({
-            "shi": "kaka"
-        })
+
+        # copy our egg
+        tmp = self._make_tmp_path()
+        source_full = self._loader.get_real_file("falafel-1.35.0-py2.7.egg")
+        tmp_src = self._connection._shell.join_path(tmp, 'insights')
+        remote_path = self._transfer_file(source_full, tmp_src)
+
+        results = merge_hash(results, self._execute_module(module_args={"egg_path": remote_path}, module_name="insights", tmp=tmp, task_vars=task_vars))
         return results
--- insights/insights.py
+++ insights/insights.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python
-
-
-def get_content():
-    return {"booger": "Content updated!"}
-
-
-def noansible():
-    import json
-    print json.dumps(get_content())
+import sys
 
 
 def main():
     from ansible.module_utils.basic import AnsibleModule
-    module = AnsibleModule({})
-    module.exit_json(**get_content())
+    module = AnsibleModule({
+        "egg_path": {"required": True, "type": "path"}
+    })
+    sys.path.append(module.params["egg_path"])
+    import falafel
+    module.exit_json(nvr=falafel.get_nvr())
 
 if __name__ == "__main__":
-    noansible()
+    main()
